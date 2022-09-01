42b855c9e945dcc997cb9a67fac8d529e371fa19
Valentyn Fediuk <valentynfediuk2001@gmail.com>
2022-09-01 09:51:37 +0300

    [transition] to next js

--- client/src/components/buttons/OutlinedBtn.props.ts
+++
@@ -1,4 +0,0 @@
-export interface OutlinedBtnProps {
-    children: string
-    sx?: any
-}
--- client/src/components/inputs/OutlinedInput.props.ts
+++
@@ -1,7 +0,0 @@
-import {ChangeEventHandler} from "react";
-
-export interface OutlinedInputProps {
-    label: string
-    type: string
-    onChange?: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>
-}
