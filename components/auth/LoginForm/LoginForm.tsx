6a1d9854aca6d2670ca119e15e90529b99152ccf
Valentyn Fediuk <valentynfediuk2001@gmail.com>
2022-09-02 15:38:36 +0300

    [added] login page

---
+++ components/auth/LoginForm/LoginForm.tsx
@@ -0,0 +1,11 @@
+import styles from './LoginForm.module.scss'
+import React from "react";
+
+export const LoginForm = () => {
+
+    return (
+        <form className={styles.form_wrapper}>
+            <h1 className={'title'}>Log in</h1>
+        </form>
+    )
+}
--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,11 +1,12 @@
 import React, {FunctionComponent} from 'react';
 import Layout from "../../layout/Layout";
+import {LoginForm} from "../../components/auth/LoginForm/LoginForm";
 
 const Login: FunctionComponent = () => {
     return (
         <div>
             <Layout>
-                login
+                <LoginForm />
             </Layout>
         </div>
     );
