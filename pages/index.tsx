1a25b0b574791a816d309f0349fca0416231a39b
Valentyn Fediuk <valentynfediuk2001@gmail.com>
2023-02-24 16:14:47 +0200

    [deploy]

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,24 @@
-import type {NextPage} from 'next'
-import {useEffect} from "react";
-import {useAppDispatch, useAppSelector} from "../hooks/redux";
-import {getUser} from "../store/reducers/ActionCreators";
+import type { NextPage } from 'next'
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../hooks/redux";
+import { getUser } from "../store/reducers/ActionCreators";
+import AuthLayout from "../layouts/AuthLayout";
+import { SignInForm } from "../components/auth/SignInForm/SignInForm";
 
 interface IUser {
-    token: string;
-    email: string;
-    id: number;
-    name: string;
+	token: string;
+	email: string;
+	id: number;
+	name: string;
 }
 
 const Home: NextPage = () => {
 
-
-    return (
-        <div>
-            fffffdddd
-        </div>
-    )
+	return (
+		<AuthLayout>
+			<SignInForm/>
+		</AuthLayout>
+	)
 }
 
 export default Home
